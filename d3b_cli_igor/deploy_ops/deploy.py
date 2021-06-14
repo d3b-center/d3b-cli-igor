0876beac545d9ff7d00e1e33702cf4305640d6c8
Alex Lubneuski <alex@d3b.center>
2021-06-14 10:08:13 -0400

    :wrench: Added deploy

--- d3b_cli_igor/deploy_ops/deploy.py
+++ d3b_cli_igor/deploy_ops/deploy.py
@@ -7,10 +7,14 @@ logger = d3b_cli_igor.common.get_logger(
     __name__, testing_mode=False, log_format="detailed"
 )
 
-def deploy_jenkinsfile(account_name, organization, region, environment):
-    d3b_cli_igor.deploy_ops.generate_config.generate(account_name, organization, region, environment) 
-    logger.info("Generating deployment script")
-    os.system('./tmp_deploy_application')
-    logger.info("Cleaning Up")
-    os.remove('./tmp_deploy_application')
 
+def deploy(account_name, organization, region, environment, config_file, mode):
+    d3b_cli_igor.deploy_ops.generate_config.generate(
+        account_name, organization, region, environment, config_file, mode
+    )
+    logger.info("Generating deployment script")
+    os.system("./tmp_deploy_application")
+
+
+#    logger.info("Cleaning Up")
+#    os.remove('./tmp_deploy_application')
--- d3b_cli_igor/deploy_ops/generate_config.py
+++ d3b_cli_igor/deploy_ops/generate_config.py
@@ -2,23 +2,28 @@ import os, sys, pathlib
 import click
 import stat
 import d3b_cli_igor.common
-import jinja2 
+import jinja2
 from jinja2 import ChoiceLoader, FileSystemLoader
 
 logger = d3b_cli_igor.common.get_logger(
     __name__, testing_mode=False, log_format="detailed"
 )
 
-def generate(account_name, organization, region, environment):
-    templateEnv = jinja2.Environment(loader=FileSystemLoader(pathlib.Path(__file__).parent.absolute()))
+
+def generate(account_name, organization, region, environment, config_file, mode):
+    templateEnv = jinja2.Environment(
+        loader=FileSystemLoader(pathlib.Path(__file__).parent.absolute())
+    )
     path = os.getcwd()
     logger.info("Checking for Jenkinsfile")
-    jenkinsfile = open(path + "/Jenkinsfile", "r")
+    jenkinsfile = open(path + "/" + config_file, "r")
     logger.info("Found Jenkinsfile")
     lines = jenkinsfile.readlines()
-    f = open("tmp_deploy_application", "w")
+    f = open("tmp_" + mode + "_application", "w")
 
     logger.info("Generating file")
+    f.write("set -e")
+    f.write("\n")
     f.write('export account_name="' + account_name + '"')
     f.write("\n")
     f.write('export region="' + region + '"')
@@ -31,6 +36,8 @@ def generate(account_name, organization, region, environment):
     f.write("\n")
     f.write("export branch=master")
     f.write("\n")
+    f.write("export mode=" + mode)
+    f.write("\n")
     for line in lines:
         if "=" in line and "shared-libraries" not in line:
             name, var = line.partition("=")[::2]
@@ -41,14 +48,14 @@ def generate(account_name, organization, region, environment):
             f.write("\n")
             f.write('export cloud_platform="aws"')
             f.write("\n")
-            f.write('export TF_VAR_organization="d3b"')
+            f.write("export TF_VAR_organization=" + organization)
             f.write("\n")
         if "aws_infra_ec2_module" in line:
             f.write('export architecture_type="aws-infra-ec2"')
             f.write("\n")
-    template = templateEnv.get_template("templates/deploy.tmpl")
-    st = os.stat('./tmp_deploy_application')
-    os.chmod("./tmp_deploy_application", st.st_mode | stat.S_IEXEC)
+    template = templateEnv.get_template("templates/deployment.tmpl")
+    st = os.stat("./tmp_" + mode + "_application")
+    os.chmod("./tmp_" + mode + "_application", st.st_mode | stat.S_IEXEC)
     output = template.render()
     f.write(output)
     f.close()
