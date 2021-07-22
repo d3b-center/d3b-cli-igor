9a24774b6fcb4b42f08b76421ec7de1494c2b95c
Alex Lubneuski <alex@d3b.center>
2021-07-22 10:38:52 -0400

    :wrench: Added removal of files

--- d3b_cli_igor/deploy_ops/deploy.py
+++ d3b_cli_igor/deploy_ops/deploy.py
@@ -1,6 +1,7 @@
 import os, sys, pathlib
 import click
-import stat
+import stat, shutil
+from pathlib import Path
 import d3b_cli_igor.common, d3b_cli_igor.deploy_ops.generate_config
 
 logger = d3b_cli_igor.common.get_logger(
@@ -11,8 +12,13 @@ def deploy(account_name, organization, region, environment, config_file, mode):
     d3b_cli_igor.deploy_ops.generate_config.generate(
         account_name, organization, region, environment, config_file, mode
     )
+
     logger.info("Executing deployment script")
     os.system("./tmp_"+mode+"_application")
 
-#    logger.info("Cleaning Up")
-#    os.remove("./tmp_"+mode+"_application")
+    logger.info("Cleaning Up")
+    os.remove("./tmp_"+mode+"_application")
+    dirpath = Path('./tmp')
+    if dirpath.exists() and dirpath.is_dir():
+        shutil.rmtree(dirpath)
+
