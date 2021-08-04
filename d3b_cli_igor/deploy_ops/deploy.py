b6df87ee6f6e626d78d4e3f7248ae70e34d7b1aa
Alex Lubneuski <alex@d3b.center>
2021-08-04 15:51:09 -0400

    :wrench: Updated build scripts

--- d3b_cli_igor/deploy_ops/deploy.py
+++ d3b_cli_igor/deploy_ops/deploy.py
@@ -14,10 +14,13 @@ def deploy(account_name, organization, region, environment, config_file, mode):
     )
     logger.info("Executing deployment script")
     exit_status = os.system("./tmp_"+mode+"_application")
+    if(exit_status != 0):
+        exit_status = 1
 
     logger.info("Cleaning Up")
-    os.remove("./tmp_"+mode+"_application")
-    dirpath = Path('./tmp')
-    if dirpath.exists() and dirpath.is_dir():
-        shutil.rmtree(dirpath)
+#    os.remove("./tmp_"+mode+"_application")
+#    dirpath = Path('./tmp')
+#    if dirpath.exists() and dirpath.is_dir():
+#        shutil.rmtree(dirpath)
+    logger.info("Exit status is : "+str(exit_status))
     sys.exit(exit_status)
