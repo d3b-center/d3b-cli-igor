d069db1f65157e7dc760023b5a014c37af5cfbdf
Alex Lubneuski <alex@d3b.center>
2021-10-06 10:28:13 -0400

    :wrench: Added ingress rules

--- d3b_cli_igor/app_ops/ecs_get_info.py
+++ d3b_cli_igor/app_ops/ecs_get_info.py
@@ -23,11 +23,14 @@ def get_info(app, environment, account, region):
     ecs_client = boto3.client('ecs', region_name=region)
     rds_client = boto3.client('rds', region_name=region)
     albs = alb_client.describe_load_balancers()
+    ec2_client = boto3.client('ec2', region_name=region)
+
     app_resource = {}
     app_resources = [] 
     alb_arns = [] 
     alb_sg = []
     rds_info={}
+
     try:
         if environment != "prd":
             rds_info=rds_client.describe_db_instances(DBInstanceIdentifier=app+"-postgres-service-rds")
@@ -38,6 +41,7 @@ def get_info(app, environment, account, region):
         logger.info(e)
     for item in albs["LoadBalancers"]:
         tags=alb_client.describe_tags(ResourceArns=[item["LoadBalancerArn"]])
+        #Comparing Tags
         for tag_desc in tags["TagDescriptions"]:
             for tag in  tag_desc["Tags"]:
                 if tag["Key"] == "Name":
@@ -45,8 +49,10 @@ def get_info(app, environment, account, region):
                 if tag["Key"] == "Environment":
                     env = tag["Value"]
             if application == app and environment == env:
+                #Get Ingress Rules for security groups
+                sg_rules=ec2_client.describe_security_groups(GroupIds=item['SecurityGroups'])["SecurityGroups"]
                 service=ecs_client.describe_services(cluster=app_cluster,services=[app+"-"+environment])['services'][0]
-                app_resource={ "app": app, "alb": str(item["LoadBalancerArn"]), "alb_sgs": str(item["SecurityGroups"]), "alb_listeners": str(alb_client.describe_listeners(LoadBalancerArn=item["LoadBalancerArn"])['Listeners']),"service_info":{"status":service['status'],"desired_count": service['desiredCount'], 'running_count': service['runningCount'], 'pending_count': service['pendingCount'], 'deployments': service['deployments']},"rds_info": rds_info }
+                app_resource={ "app": app, "alb": str(item["LoadBalancerArn"]), "alb_sgs": str(item["SecurityGroups"]),"sg_rules": str(sg_rules), "alb_listeners": str(alb_client.describe_listeners(LoadBalancerArn=item["LoadBalancerArn"])['Listeners']),"service_info":{"status":service['status'],"desired_count": service['desiredCount'], 'running_count': service['runningCount'], 'pending_count': service['pendingCount'], 'deployments': service['deployments']},"rds_info": rds_info }
                 app_resources.append(app_resource)
     pretty_print(app_resources)
 
