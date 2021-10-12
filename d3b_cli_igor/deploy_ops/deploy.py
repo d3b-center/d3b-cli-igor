629acf70161fae36560df73bb0b9181c0a599ab3
Alex Lubneuski <alex@d3b.center>
2021-10-12 14:26:31 -0400

    :bug: Fixed buggy if statement

--- d3b_cli_igor/deploy_ops/deploy.py
+++ d3b_cli_igor/deploy_ops/deploy.py
@@ -19,7 +19,7 @@ def deploy(account_name, organization, region, environment, config_file, mode, d
         exit_status = 1
     if not debug:  
         logger.info("Cleaning Up")
-        os.remove("./tmp_" + mode + "_application")
+#        os.remove("./tmp_" + mode + "_application")
         dirpath = Path("./tmp")
         if dirpath.exists() and dirpath.is_dir():
             shutil.rmtree(dirpath)
