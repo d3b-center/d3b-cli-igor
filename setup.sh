b2d09746bc8685f4ed684baf03e2a5810b3bdc9c
Alex Lubneuski <lubneuskia@chop.edu>
2023-10-30 11:37:39 -0400

    :bug: Updated version of wheel package

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 pip3 install wheel
 pip3 install --upgrade pip
 python3 setup.py bdist_wheel --universal
-pip3 install --force-reinstall dist/d3b_cli_igor-0.1-py2.py3-none-any.whl
+pip3 install --force-reinstall dist/d3b_cli_igor-0.2-py2.py3-none-any.whl
